12823a7b57e3e1b8746667dda8939b5f7c311232
txzm <txzm2018@gmail.com>
2018-08-01 17:30:33 +0800

    add normalized bezier params

---
+++ src/logic/trace/BezierBuilder.js
@@ -0,0 +1,39 @@
+import BezierTrace from './BezierTrace';
+import Vec2 from '../utils/vec2';
+
+class BezierBuilder{
+
+    /**
+     * points中除start和end是真实坐标，其他都是标准化的
+     * @static
+     * @param {Array<vec2>} points
+     * @param {number} duration -帧数
+     * @memberof BezierBuilder
+     */
+    static getTraceFromNomalize(points, duration){
+        const start = points[0];
+        const end = points[points.length - 1];
+        const delta = end.sub(start);
+        const length = delta.length();
+        if(length<=0){
+            return new BezierTrace([start, end]);
+        }
+        const sintheta = delta.y / length;
+        const costheta = delta.x / length;
+        for(let i = 1; i < points.length - 1; i++){
+            const point = points[i];
+            const tp = point.mul(length);
+            const x = tp.x * costheta + tp.y * sintheta;
+            const y = tp.x * -sintheta + tp.y * costheta;
+            points[i] = new Vec2(x, y);
+        }
+        return new BezierTrace(points, duration);
+    }
+
+    static getTraceFRomNormal(points, duration){
+        return new BezierTrace(points, duration);
+    }
+
+}
+
+export default BezierBuilder;
--- src/test/trace/BezierTraceTest.js
+++ src/test/trace/BezierTraceTest.js
@@ -1,4 +1,5 @@
 import BezierTrace from '../../logic/trace/BezierTrace';
+import BezierBuilder from '../../logic/trace/BezierBuilder';
 import Vec2 from '../../logic/utils/vec2';
 
 const points = [];
@@ -6,9 +7,16 @@ points.push(new Vec2(0, 0));
 points.push(new Vec2(100, 100));
 points.push(new Vec2(200, 0));
 
-const trace = new BezierTrace(points, 20);
+let trace = new BezierTrace(points, 20);
 
-console.log('vec2:', new Vec2(0, 5));
+console.log('direct trace print:');
 for(let i = 0; i < 20; i++){
+    console.log(trace.nextPos());
+}
+
+points[1] = new Vec2(0.5, 0.5);
+trace = BezierBuilder.getTraceFromNomalize(points, 20);
+console.log('builder trace print:');
+for (let i = 0; i < 20; i++) {
     console.log(trace.nextPos());
 }
