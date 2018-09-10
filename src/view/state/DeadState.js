5003c5ede65c2e1cecb9d2f0873352c3b27d393b
txzm <txzm2018@gmail.com>
2018-09-10 11:24:29 +0800

    fix dead hp bar

--- src/view/state/DeadState.js
+++ src/view/state/DeadState.js
@@ -10,6 +10,7 @@ class DeadState extends BaseState {
     onEnter(sm) {
         super.onEnter(sm);
         sm.viewEntity.pauseAnim();
+        sm.viewEntity.refreshHpBar();
         sm.viewEntity.view.runAction(cc.sequence(cc.fadeOut(2), cc.callFunc(()=>{
             sm.viewEntity.onDead();
         })));
